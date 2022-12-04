0924cd50992a96faf47887c902b79aebae5ba101
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-12-04 12:37:23 +0000

    typo fix

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
-axios.defaults.baseURL = 'https://api-practice-for-cozy.herokuapp.com/';
+axios.defaults.baseURL = "https://api-practice-for-cozy.herokuapp.com/";
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
