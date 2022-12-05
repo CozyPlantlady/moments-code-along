f3a64364e10f296bb34edb03341b00cb1cb97afc
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-12-05 19:47:20 +0000

    Challenge:Authentication:creating the SignIn form

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
 axios.defaults.baseURL = "https://api-practice-for-cozy.herokuapp.com/";
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
