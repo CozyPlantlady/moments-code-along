afa561eaaf733905f8c6314a05e3d01dd196203d
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-12-07 11:36:42 +0000

    Adjusting NavBar for LoggedIn/ LoggedOut

--- src/context/CurrentUserContext.js
+++ src/context/CurrentUserContext.js
@@ -6,8 +6,8 @@ import { useHistory } from 'react-router';
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
 
-export const useCurrentUser = () => useContext(CurrentUserContext)
-export const useSetCurrentUser = () => useContext(SetCurrentUserContext)
+export const useCurrentUser = () => useContext(CurrentUserContext);
+export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
 
 export const CurrentUserProvider = ({children}) => {
     const [currentUser, setCurrentUser] = useState(null);
@@ -15,7 +15,7 @@ export const CurrentUserProvider = ({children}) => {
 
     const handleMount = async () => {
       try {
-        const {data} = await axiosRes.get('dj-rest-auth/user/');
+        const { data } = await axiosRes.get('dj-rest-auth/user/');
         setCurrentUser(data)
       } catch(err){
         console.log(err)
@@ -30,35 +30,35 @@ export const CurrentUserProvider = ({children}) => {
         axiosReq.interceptors.request.use(
           async (config) => {
             try {
-              await axios.post('/dj-rest-auth/token/refresh/')
-            } catch(err){
+              await axios.post('/dj-rest-auth/token/refresh/');
+            } catch (err) {
               setCurrentUser((prevCurrentUser) => {
                 if (prevCurrentUser) {
-                  history.push('/signin')
+                  history.push('/signin');
                 }
-                return null
+                return null;
               })
-              return config
+              return config;
             }
-            return config
+            return config;
           },
           (err) => {
             return Promise.reject(err);
           }
-        )
+        );
         axiosRes.interceptors.response.use(
             (response) => response,
             async (err) => {
                 if (err.response?.status === 401){
                   try{
-                    await axios.post('/dj-rest-auth/token/refresh/')
+                    await axios.post('/dj-rest-auth/token/refresh/');
                   } catch(err){
                     setCurrentUser(prevCurrentUser => {
                       if (prevCurrentUser){
-                        history.push('/signin')
+                        history.push('/signin');
                       }
-                      return null
-                    })
+                      return null;
+                    });
                   }
                   return axios(err.config)
                 }
@@ -73,6 +73,5 @@ export const CurrentUserProvider = ({children}) => {
                 {children}
             </SetCurrentUserContext.Provider>
         </CurrentUserContext.Provider>
-    )
-
+    );
 };
