dcc3d62344136ae10bcaece77335824a840cc7f8
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-12-07 11:58:07 +0000

    Navbar toggle custom hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,20 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
-import React from 'react';
+import React  from 'react';
 import logo from '../assets/logo.png';
 import styles from '../styles/NavBar.module.css';
 import { NavLink } from 'react-router-dom'
 import { useCurrentUser, useSetCurrentUser } from "../context/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
 
+    const { expanded, setExpanded, ref } = useClickOutsideToggle();
+
+    
     const handleSignOut = async () => {
         try {
             await axios.post("dj-rest-auth/logout/");
@@ -75,7 +79,7 @@ const NavBar = () => {
         </>;
 
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
             <Container>
                 <NavLink to="/">
                     <Navbar.Brand>
@@ -83,7 +87,10 @@ const NavBar = () => {
                     </Navbar.Brand>
                 </NavLink>
                 {currentUser && addPostIcon}
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle 
+                ref={ref}
+                    onClick={() => setExpanded(!expanded)} 
+                    aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
                         <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
