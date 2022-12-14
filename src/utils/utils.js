b7929700ec62b5663b97081d8667d541852a66cc
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-12-14 18:04:23 +0000

    Cleaning up errors

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,4 @@
+import jwtDecode from "jwt-decode";
 import { axiosReq } from "../api/axiosDefaults"
 
 
@@ -48,4 +49,17 @@ export const unfollowHelper = (profile, clickedProfile) => {
         }
     :
         profile;
-}
+};
+
+export const setTokenTimestamp = (data) => {
+    const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp
+    localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp)
+};
+
+export const shouldRefreshToken = () => {
+    return !!localStorage.getItem('refreshTokenTimestamp')
+};
+
+export const removeTokenTimestamp = () => {
+    localStorage.removeItem('refreshTokenTimestamp')
+};
